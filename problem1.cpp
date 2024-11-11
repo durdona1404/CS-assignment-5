b2681d65d3d431b78c1d7c9a903b698d4b38b132
Valiqulova Durdona <d.valiqulova@newuu.uz>
2024-11-11 21:51:15 +0500

    problem1

--- problem1.cpp
+++ problem1.cpp
@@ -8,3 +8,17 @@ int sum(int array1[], int array2[], int n) {
     }
     return sum;
 }
+
+int main() {
+    int n;
+    cin >> n;
+    int array1[n];
+    int array2[n];
+    for (int i = 0; i < n; i++) {
+        cin >> array1[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> array2[i];
+    }
+    cout << sum(array1, array2, n);
+}
